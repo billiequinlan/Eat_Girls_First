13b817da65763991fb96f1566b1d5fedf36059b3
billiequinlan <billieq@hotmail.co.uk>
2017-09-12 18:04:04 +0100

    finally add jquery to img

--- aboutus.js
+++ aboutus.js
@@ -1,10 +1,18 @@
+
 $(document).ready(function(){
-		
-$("#elizabethcopy").hide()
+	$("#elizabethcopy").hide();
 		$("#photo1").hover(function(){
 			$("#elizabethcopy").toggle();
+	});
 
+	$("#siancopy").hide();
+		$("#photo2").hover(function(){
+			$("#siancopy").toggle();
+
+	});
+
+	$("#billiecopy").hide();
+		$("#photo3").hover(function(){
+			$("#billiecopy").toggle();
+		});
 })
-
-
-});
