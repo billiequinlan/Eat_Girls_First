adb8e2c3847abf90f3f6a01633075e1e437d3de6
billiequinlan <billieq@hotmail.co.uk>
2017-09-13 18:57:16 +0100

    final edits

--- consultation.js
+++ consultation.js
@@ -3,6 +3,14 @@ $(function(){
 
 alert("Free Health Consultation, sign up below");
 
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 });
 
